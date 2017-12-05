8c32e9f712cedfeeec0f59cb38be8eb048fc9b48
Pat Neel <patrick.neel@colorado.edu>
2017-12-04 18:08:25 -0700

    added eslint and testing environemnts

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { shallow, mount } from 'enzyme';
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+describe('App Test', () => {
+
+  it('should be defined', () => {
+  const renderedApp = shallow(<App />);
+
+  expect(renderedApp).toBeDefined();
+    
+  });
+  
 });
