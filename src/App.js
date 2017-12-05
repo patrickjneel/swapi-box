da00f9b420d106880cb7c8094a14e68a074d2923
Pat Neel <patrick.neel@colorado.edu>
2017-12-05 13:12:39 -0700

    added componentDidMount for scroll

--- src/App.js
+++ src/App.js
@@ -3,6 +3,25 @@ import './App.css';
 import Scrolling from './components/scrolling-text/Scrolling-text';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      scrollingText: ''
+    }
+  }
+
+  async componentDidMount() {
+    const randFilm = Math.floor(Math.random() * (7) + 1);
+    const fetchedData = await fetch(`https://swapi.co/api/films/${randFilm}/`)
+    const data = await fetchedData.json()
+    const title = data.title
+    const crawl = data.opening_crawl
+    const episodeNumber = data.episode_id
+    
+    this.setState({ title, crawl, episodeNumber })
+  }
+
+  
   render() {
     return (
       <div className="App">
