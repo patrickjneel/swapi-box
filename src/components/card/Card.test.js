8dcaa30855e69ffb5ea47f2fe09d34fcba1819d4
Pat Neel <patrick.neel@colorado.edu>
2017-12-05 16:02:08 -0700

    added card component with test set up

---
+++ src/components/card/Card.test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { shallow, mount } from 'enzyme';
+import Card from './Card';
+
+describe('Card Test', () => {
+  it('should be defined', () => {
+    const renderedCard = shallow(<Card />);
+
+    expect(renderedCard).toBeDefined();
+  })
+})
