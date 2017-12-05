c0868dd17e61e8ed97c4cb82e9b39c3534dfaa0b
Pat Neel <patrick.neel@colorado.edu>
2017-12-04 18:20:30 -0700

    added Scrolling component and basic component file structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
+import Scrolling from './components/scrolling-text/Scrolling-text';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Scrolling />
        Swapi-Box
       </div>
     );
