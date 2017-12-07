dabc6599c204453df1594c965c2ad45746f33f2d
Pat Neel <patrick.neel@colorado.edu>
2017-12-07 09:58:40 -0700

    conditional rendering partially set up for data

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import './card.css';
 
-const Card = ({name, planet, population, species}) => {
+const Card = ({name, planet, population, species, itemData, location,}) => {
+ console.log(itemData)
   return (
     <div className="card">
       <h3>{name}</h3>
       <h4>{planet}</h4>
       <h4>{population}</h4>
       <h5>{species}</h5>
-      {species && <h5>species</h5>}
+      {
+        location === 'planets' &&
+        <h2>{itemData.terrrain}</h2>
+      }
     </div>
   )
 }
