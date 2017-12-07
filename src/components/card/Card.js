2f9dd0dd4a8b0cb628879ab0cfb2dc6e6f29ed58
Pat Neel <patrick.neel@colorado.edu>
2017-12-07 14:01:12 -0700

    finished styling cards

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -4,13 +4,15 @@ import Imperial_Emblem from '../../Imperial_Emblem.svg';
 const Card = ( {itemData, location, addFavorite} ) => {
 
   let mapped;
+
   if(location === 'planets') {
     mapped = itemData.residents.map(people => {
       return <div>{people}</div>
-      if(!people) {
-        return <div>N/A</div>
-      }
+
     })
+    if(!itemData.residents){
+      return 0;
+    }
   }
 
   return (
@@ -24,27 +26,25 @@ const Card = ( {itemData, location, addFavorite} ) => {
       { 
         location === 'people' &&
         <div className="card-data">
-          <h4>Planet: {itemData.homeworld}</h4>
-          <h4>Population: {itemData.population}</h4>
-          <h4>Species: {itemData.species}</h4>
+          <h4>Planet:</h4> <h5>{itemData.homeworld}</h5>
+          <h4>Population:</h4> <h5>{itemData.population}</h5>
+          <h4>Species:</h4> <h5>{itemData.species}</h5>
         </div>
       }
       {
         location === 'planets' &&
         <div className="card-data">
-          <h4>Terrain: {itemData.terrrain}</h4>
-          <h4>Climate: {itemData.climate}</h4>
-          <h4>Residents: {mapped}</h4>
-          
-          
+          <h4>Terrain:</h4> <h5>{itemData.terrrain}</h5>
+          <h4>Climate:</h4> <h5>{itemData.climate}</h5>
+          <h4>Residents:</h4> <h5>{mapped}</h5>
         </div>
       }
       {
         location === 'vehicles' &&
         <div className="card-data">
-         <h4>Model: {itemData.model}</h4>
-         <h4>Class: {itemData.class}</h4>
-         <h4>Passengers: {itemData.passengers}</h4>
+         <h4>Model:</h4> <h5>{itemData.model}</h5>
+         <h4>Class:</h4> <h5>{itemData.class}</h5>
+         <h4>Passengers:</h4> <h5>{itemData.passengers}</h5>
 
         </div>
       }
