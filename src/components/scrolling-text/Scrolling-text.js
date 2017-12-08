b582dd4e38ccda8dc7658eaef26bdc18f5aaa5fe
Pat Neel <patrick.neel@colorado.edu>
2017-12-07 19:57:46 -0700

    eslint

--- src/components/scrolling-text/Scrolling-text.js
+++ src/components/scrolling-text/Scrolling-text.js
@@ -3,25 +3,26 @@ import './scrolling-text.css';
 
 const Scrolling = ({film}) => {
   const { title, crawl, episodeNumber } = film;
-  const epNumerial = {
-      1: 'I',
-      2: 'II',
-      3: 'III',
-      4: 'IV',
-      5: 'V',
-      6: 'VI',
-      7: 'VII'
-    }
+  const epNumerial = {  
+    1: 'I',
+    2: 'II',
+    3: 'III',
+    4: 'IV',
+    5: 'V',
+    6: 'VI',
+    7: 'VII'
+  };
+
   return (
     <div className="scrolling-area">
-     <div className="scroll-text">
+      <div className="scroll-text">
         <h3>{title}</h3>
-       <h5>EPISODE {epNumerial[episodeNumber]}</h5>
+        <h5>EPISODE {epNumerial[episodeNumber]}</h5>
         <h4>{crawl}</h4>
-     </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default Scrolling;
 
