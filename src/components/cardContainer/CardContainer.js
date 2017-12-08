d0c37095de827d904fef5f4f7190f3d4cca955a4
Pat Neel <patrick.neel@colorado.edu>
2017-12-07 20:03:41 -0700

    eslint

--- src/components/cardContainer/CardContainer.js
+++ src/components/cardContainer/CardContainer.js
@@ -5,18 +5,18 @@ import './cardContainer.css';
 const CardContainer = ({itemData, location, addFavorite}) => {
   const persons = itemData.map(card => {
     return <Card
-            itemData={card}
-            key={card.name}
-            addFavorite={addFavorite}
-            location={location}
-           />
-  })
+      itemData={card}
+      key={card.name}
+      addFavorite={addFavorite}
+      location={location}
+    />;
+  });
   
   return (
-     <div className="container">
-     {persons}
+    <div className="container">
+      {persons}
     </div>
-  )
-}
+  );
+};
 
 export default CardContainer;
