a7bac23f79a85a48f29541a5f0660d058c20b3e8
Pat Neel <patrick.neel@colorado.edu>
2017-12-09 16:18:00 -0700

    added tests to card container

--- src/App.js
+++ src/App.js
@@ -50,7 +50,6 @@ class App extends Component {
   }
 
   render() {
-    console.log(this.state.film)
     
     const arrayToRender = this.state.location;
 
--- src/components/cardContainer/CardContainer.js
+++ src/components/cardContainer/CardContainer.js
@@ -4,7 +4,7 @@ import './cardContainer.css';
 import PropTypes from 'prop-types';
 
 const CardContainer = ({itemData, location, updateFavorite, favorites}) => {
-  
+ 
   const persons = itemData.map(card => {
     let favoritedCard = favorites.find(favorite => favorite.name === card.name)
     let imperialClass = favoritedCard ? 'imperial favorited' : 'imperial';  
