e1be3e507dc26b8d87a0f95662ef67654fb570c5
Pat Neel <patrick.neel@colorado.edu>
2017-12-10 10:33:46 -0700

    added two more tests for app

--- src/App.test.js
+++ src/App.test.js
@@ -10,5 +10,36 @@ describe('App Test', () => {
   expect(renderedApp).toBeDefined();
     
   });
+
+  it('should render the correct components', () => {
+    const renderedApp = shallow(<App />)
+    const foundHeader = renderedApp.find('Header');
+    const foundConatiner = renderedApp.find('CardContainer')
+    const foundScroll = renderedApp.find('Scrolling')
+
+    expect(foundHeader.length).toEqual(1)
+    expect(foundConatiner.length).toEqual(1)
+    expect(foundScroll.length).toEqual(1)
+  })
+  
+  it('should have a default state', () => {
+    const renderedApp = shallow(<App />)
+    const expectedFilm = {};
+    const expectedPeople = [];
+    const expectedVehicles = [];
+    const expectedPlanets = [];
+    const expectedLocation = 'people' ;
+    const expectedFavorites = [] 
+
+    expect(renderedApp.state('film')).toEqual(expectedFilm)
+    expect(renderedApp.state('people')).toEqual(expectedPeople)
+    expect(renderedApp.state('vehicles')).toEqual(expectedVehicles)
+    expect(renderedApp.state('planets')).toEqual(expectedPlanets)
+    expect(renderedApp.state('location')).toEqual(expectedLocation)
+    expect(renderedApp.state('favorites')).toEqual(expectedFavorites)
+
+  })
+
+  
   
 });
